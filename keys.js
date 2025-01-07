ce995c7f3be042bba26c35f83b0c1206783b070d
adivs1102 <aadityavir11@gmail.com>
2025-01-07 16:24:40 +0530

    first commit

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI:
+        "mongodb+srv://adirox:adirox123@cluster0.xruox8y.mongodb.net/test2?retryWrites=true&w=majority&appName=Cluster0",
+    JWT_SECRET: "MYSECRET34329",
+};
